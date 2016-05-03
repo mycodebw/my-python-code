195ead28675198bb97211f9d7689fd13dbe60fb2
mycodebw <bwbw_48@hotmail.com>
2016-05-03 05:53:55 -0400

    update

--- score.py
+++ score.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python -w
 #coding=utf-8
-score = input('please score is: ')
 
-while score > 0 :
+while True :
+	score = input('please score is: ')
 	if score >= 60 and score < 70:
 		print "score  is pass"
-		break
+		continue
 	elif score >= 70 and score < 85:
 		print "score is good"
-		break
+		continue
 	elif score >= 85 and score < 100: 
 		print "score is excellent"
-		break
+		continue
 	elif score < 60:
 		print "score is failed"
-		break 
+		continue 
