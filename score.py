b4bd743cba276b558e8a490531fddc133822117c
mycodebw <bwbw_48@hotmail.com>
2016-05-03 05:49:33 -0400

    update

--- score.py
+++ score.py
@@ -6,12 +6,12 @@ while score > 0 :
 	if score >= 60 and score < 70:
 		print "score  is pass"
 		break
-	elif score >= 70 and score <= 85:
+	elif score >= 70 and score < 85:
 		print "score is good"
 		break
-	elif score >= 85 and score <= 100: 
+	elif score >= 85 and score < 100: 
 		print "score is excellent"
 		break
-	elif score <= 60:
+	elif score < 60:
 		print "score is failed"
 		break 
